b2bcd98ef6b9de09d97ee481af3ca4311a4f033f
Assz <renanguides@hotmail.com>
2023-03-29 00:38:48 -0400

    adicionado websocket no createOrder

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
-
+import { io } from '../../../index';
 import { Order } from '../../models/Order';
 
 export async function createOrder(req: Request,res: Response){
   try{
     const { table,products } = req.body;
 
-    const orders = await Order.create({table, products});
+    const order = await Order.create({table, products});
+    const orderDetails = await order.populate('products.product');
+    io.emit('order@new', orderDetails);
 
-    res.status(201).json(orders);
+    res.status(201).json(order);
   }catch (error){
     console.log(error);
     res.status(500);
